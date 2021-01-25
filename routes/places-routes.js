fd10bec7528dae480b228bee7480006352e0989c
Alexandre Queiroz <xandeq@gmail.com>
2021-01-25 15:39:03 -0300

    Segundo Commit

--- routes/places-routes.js
+++ routes/places-routes.js
@@ -1,39 +1,44 @@
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
 
 const DUMMY_PLACES = [
-    {
-        id: 'p1',
-        title: 'Empire State Building',
-        description: 'One of the most famous skyscrappers in the world',
-        location: {
-            lat: 40.7484445,
-            lng: -73.9878531
-        },
-        address: '20 W 34th St, New York, NY 10001, Estados Unidos',
-        creator: 'u1'
-    }
-]
+  {
+    id: "p1",
+    title: "Empire State Building",
+    description: "One of the most famous skyscrappers in the world",
+    location: {
+      lat: 40.7484445,
+      lng: -73.9878531,
+    },
+    address: "20 W 34th St, New York, NY 10001, Estados Unidos",
+    creator: "u1",
+  },
+];
 
 // Obter places pelo iD do place
-router.get('/place/:pid', (req,res,next) => {
-    const placeId = req.params.pid;
-    const place = DUMMY_PLACES.find(p => {
-        return p.id === placeId;
-    })
-    res.json({place});
+router.get("/:pid", (req, res, next) => {
+  const placeId = req.params.pid;
+  const place = DUMMY_PLACES.find((p) => {
+    return p.id === placeId;
+  });
+
+  if(!place){
+      res.status(404).json({ message: 'Could not find a place for the provided ID'});
+  }
+
+  res.json({ place });
 });
 
 // Obter places pelo iD do usuario
-router.get('/user/:uid', (req,res,next) => {
-    const userId = req.params.uid;
+router.get("/user/:uid", (req, res, next) => {
+  const userId = req.params.uid;
 
-    const place = DUMMY_PLACES.find(p => {
-        return p.creator === userId;
-    })
-    res.json({place});
-})
+  const place = DUMMY_PLACES.find((p) => {
+    return p.creator === userId;
+  });
+  res.json({ place });
+});
 
 // COm isso podemos usar no app.js
 module.exports = router;
