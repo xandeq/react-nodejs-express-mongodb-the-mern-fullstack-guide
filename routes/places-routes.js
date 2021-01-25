2c591864d72d87a6badfc3d44d4a674ba2162a46
Alexandre Queiroz <xandeq@gmail.com>
2021-01-25 19:56:45 -0300

    Fim da aula

--- routes/places-routes.js
+++ routes/places-routes.js
@@ -1,30 +1,30 @@
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
 
 const DUMMY_PLACES = [
   {
-    id: 'p1',
-    title: 'Empire State Building',
-    description: 'One of the most famous sky scrapers in the world!',
+    id: "p1",
+    title: "Empire State Building",
+    description: "One of the most famous sky scrapers in the world!",
     location: {
       lat: 40.7484474,
-      lng: -73.9871516
+      lng: -73.9871516,
     },
-    address: '20 W 34th St, New York, NY 10001',
-    creator: 'u1'
-  }
+    address: "20 W 34th St, New York, NY 10001",
+    creator: "u1",
+  },
 ];
 
-router.get('/:pid', (req, res, next) => {
+router.get("/:pid", (req, res, next) => {
   const placeId = req.params.pid; // { pid: 'p1' }
 
-  const place = DUMMY_PLACES.find(p => {
+  const place = DUMMY_PLACES.find((p) => {
     return p.id === placeId;
   });
 
   if (!place) {
-    const error = new Error('Could not find a place for the provided id.');
+    const error = new Error("Could not find a place for the provided id.");
     error.code = 404;
     throw error;
   }
@@ -32,15 +32,15 @@ router.get('/:pid', (req, res, next) => {
   res.json({ place }); // => { place } => { place: place }
 });
 
-router.get('/user/:uid', (req, res, next) => {
+router.get("/user/:uid", (req, res, next) => {
   const userId = req.params.uid;
 
-  const place = DUMMY_PLACES.find(p => {
+  const place = DUMMY_PLACES.find((p) => {
     return p.creator === userId;
   });
 
   if (!place) {
-    const error = new Error('Could not find a place for the provided user id.');
+    const error = new Error("Could not find a place for the provided user id.");
     error.code = 404;
     return next(error);
   }
