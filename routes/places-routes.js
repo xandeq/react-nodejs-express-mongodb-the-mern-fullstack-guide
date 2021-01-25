f25c50425b759fb1c921039e45a593040f7884cf
Alexandre Queiroz <xandeq@gmail.com>
2021-01-25 18:26:29 -0300

    Deu erro

--- routes/places-routes.js
+++ routes/places-routes.js
@@ -1,44 +1,51 @@
-const express = require("express");
+const express = require('express');
 
 const router = express.Router();
 
 const DUMMY_PLACES = [
   {
-    id: "p1",
-    title: "Empire State Building",
-    description: "One of the most famous skyscrappers in the world",
+    id: 'p1',
+    title: 'Empire State Building',
+    description: 'One of the most famous sky scrapers in the world!',
     location: {
-      lat: 40.7484445,
-      lng: -73.9878531,
+      lat: 40.7484474,
+      lng: -73.9871516
     },
-    address: "20 W 34th St, New York, NY 10001, Estados Unidos",
-    creator: "u1",
-  },
+    address: '20 W 34th St, New York, NY 10001',
+    creator: 'u1'
+  }
 ];
 
-// Obter places pelo iD do place
-router.get("/:pid", (req, res, next) => {
-  const placeId = req.params.pid;
-  const place = DUMMY_PLACES.find((p) => {
+router.get('/:pid', (req, res, next) => {
+  const placeId = req.params.pid; // { pid: 'p1' }
+
+  const place = DUMMY_PLACES.find(p => {
     return p.id === placeId;
   });
 
-  if(!place){
-      res.status(404).json({ message: 'Could not find a place for the provided ID'});
+  if (!place) {
+    const error = new Error('Could not find a place for the provided id.');
+    error.code = 404;
+    throw error;
+  }
+
+  res.json({ place }); // => { place } => { place: place }
+});
+
+router.get('/user/:uid', (req, res, next) => {
+  const userId = req.params.uid;
+
+  const place = DUMMY_PLACES.find(p => {
+    return p.creator === userId;
+  });
+
+  if (!place) {
+    const error = new Error('Could not find a place for the provided user id.');
+    error.code = 404;
+    return next(error);
   }
 
   res.json({ place });
 });
 
-// Obter places pelo iD do usuario
-router.get("/user/:uid", (req, res, next) => {
-  const userId = req.params.uid;
-
-  const place = DUMMY_PLACES.find((p) => {
-    return p.creator === userId;
-  });
-  res.json({ place });
-});
-
-// COm isso podemos usar no app.js
 module.exports = router;
