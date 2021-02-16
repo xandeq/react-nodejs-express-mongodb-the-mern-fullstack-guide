5c364ac7df085e4f931fc03883a41834ddb29b11
Alexandre Queirozm <xandeq@gmail.com>
2021-02-16 08:45:37 -0300

    Lição 110  Configurando MongoDB

---
+++ mongo.js
@@ -0,0 +1,14 @@
+const MongoClient = require('mongodb').MongoClient();
+
+const url = 'mongodb+srv://xandeq:alexandre10@cluster0.5jbns.mongodb.net/products_test?retryWrites=true&w=majority';
+
+const createProduct = async (req, res, next) => {
+
+}
+
+const getProducts = async (req, res, next) => {
+
+}
+
+exports.createProduct = createProduct;
+exports.getProducts = getProducts;
