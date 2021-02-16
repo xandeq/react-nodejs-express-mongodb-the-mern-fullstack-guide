480f74d33f298bc311e4215fdb8d3b198a19869a
Alexandre Queirozm <xandeq@gmail.com>
2021-02-16 09:17:20 -0300

    mongo

--- mongo.js
+++ mongo.js
@@ -3,7 +3,20 @@ const MongoClient = require('mongodb').MongoClient();
 const url = 'mongodb+srv://xandeq:alexandre10@cluster0.5jbns.mongodb.net/products_test?retryWrites=true&w=majority';
 
 const createProduct = async (req, res, next) => {
+    const newProduct = {
+        name: req.body.name,
+        price: req.body.price
+    };
 
+    const client = new MongoClient(url);
+
+    try {
+        await client.connect();
+        const db = client.db();
+        const result = db.collection('product')
+    } catch (error) {
+        res.json({ message: 'Could not store data'});
+    }
 }
 
 const getProducts = async (req, res, next) => {
